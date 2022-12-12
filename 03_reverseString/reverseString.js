2064e3b20bb733d9aca8b428d7df6eea796be1f4
PizzasaurusR <ramizissel@gmail.com>
2022-12-12 15:50:31 +0200

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let outputStr = '';
+    for (i=0; i<= (str.length); i++){
+        outputStr += str.substr(str.length-i,1)
+    }
+    return outputStr;
 };
 
 // Do not edit below this line
