dad5caf336e3c6e1953470dcfd56db06563ef67b
PizzasaurusR <ramizissel@gmail.com>
2022-12-12 17:00:33 +0200

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    
+    if (firstNum > secondNum){
+    const temp = firstNum;
+    firstNum = secondNum;
+    secondNum = temp;
+    }
 
+    if (firstNum < 0 || secondNum < 0) return 'ERROR';
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) return 'ERROR';
+   let sum = 0;
+   for (let i = firstNum; i <= secondNum; i++) {
+    sum += i;
+   }
+
+    return sum;
 };
 
 // Do not edit below this line
